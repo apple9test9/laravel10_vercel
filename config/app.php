6dc3b66412fef915ea4ef72451dbf3f8b9c6b5d1
apple <apple9test9@gmail.com>
2024-09-08 19:40:26 +0900

    app.php modefy

--- config/app.php
+++ config/app.php
@@ -55,9 +55,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laravel10-vercel-beryl.vercel.app'),
 
-    'asset_url' => env('ASSET_URL'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
