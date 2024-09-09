6ac5b96ead5aca2aef785a83b1fed755c8b093c3
apple <apple9test9@gmail.com>
2024-09-09 22:50:57 +0900

    test12

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/', // または 'https://your-domain.com/' デプロイ先のURL
+    base: 'https://laravel10-vercel-beryl.vercel.app',
     plugins: [
         laravel({
             input: [
