58598cab9e97d38a4e75137bf6ad53ea8ac3a63b
apple <apple9test9@gmail.com>
2024-09-10 00:39:43 +0900

    test33

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laravel10-vercel-beryl.vercel.app'),
 
     'asset_url' => env('ASSET_URL'),
 
